6abc965339071db4c171f6d16a75ae03e22f3469
unknown <you@example.com>
2020-05-22 12:23:36 +0200

    Second commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,12 +6,10 @@ const { JWT_SECRET } = config;
 module.exports = (req, res, next) => {
   const token = req.header("x-auth-token");
 
-  // Check for token
   if (!token)
     return res.status(401).json({ msg: "No token, authorizaton denied" });
 
   try {
-    // Verify token
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
     next();
